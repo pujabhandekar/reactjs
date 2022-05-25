9d1637ab2b5f92d1b8569e98253168b250bde08b
puja <bhandekaranitha@gmail.com>
2022-05-25 18:43:17 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import React,{useState} from 'react'
+import Products from "./Products"
 
-import { increment,decrement } from './redux/reducer'
-import {useSelector,useDispatch} from 'react-redux'
+const App = () => {
+  const [search,setSearch]=useState()
+  const [data,setData]=useState([])
 
-function App(){
-  const count=useSelector((state)=>state.counter1.value)
-  const dispatch=useDispatch()
-return(
-  <div className='App'>
-    <h1>redux toolkit</h1>
-    <button onClick={()=>dispatch(increment())}>increment</button>
-    <span>{count}</span>
-    <button onClick={()=>dispatch(decrement())}>decrement</button>
-
-  </div>
+  const YOUR_APP_ID="adb99c26"
+  const  YOUR_APP_KEY= "6c399f456ac882ba77ebd80b04fcfb1c"	
+const submitHandler=(e)=>{
+e.preventDefault();
+console.log(search);
+fetch(`https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`).then(
+  response =>response.json()
+).then(
+ data =>setData(data.hits)
 )
+}
+
+  return (
+    <div>
+      <center>
+        <h3>Food Recipe App</h3>
+        <form onSubmit={submitHandler} >
+
+          <input type="text" value={search} onChange={(e)=>{setSearch(e.target.value)}}/> <br/><br/>
+          <input type="submit" className='btn btn-primary'/>
+        </form><br/>
+        {data.length>1?<Products data={data}/>:null}
+      </center>
+    </div>
+  )
 }
 
 export default App
